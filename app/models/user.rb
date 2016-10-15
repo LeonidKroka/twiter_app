15927c6bc948ff5431cda654635d4bcb504a9a84
Leonid Kroka <kroka_l@ukr.net>
2016-10-15 16:05:24 +0300

    Log_in and log_out by users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  before_create :create_remember_token
   before_save { email.downcase! }
 
   validates :name, presence: true, length: { maximum: 50 }
@@ -8,4 +9,18 @@ class User < ApplicationRecord
   validates :password, length: { minimum: 6 }
 
   has_secure_password
+
+  def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.encrypt(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  private
+
+    def create_remember_token
+      self.remember_token = User.encrypt(User.new_remember_token)
+    end
 end
